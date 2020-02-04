edeb4b33630ef47d7665d8b8803aedd7c8a535f0
ravindra <ravindra@ravindras-MacBook-Pro.local>
2020-02-04 08:37:17 +0530

    commit1

---
+++ bubbleSort.py
@@ -0,0 +1,19 @@
+def bubbleSort(A):
+    n = len(A)
+    for j in range (1,n):
+        for i in range (0,(n-j)):
+            if A[i] > A[i+1]:
+                swap(i,(i+1),A)
+    return A
+
+
+def swap(i,j,A):  # swap positions between two elements of an array and return the matrix
+    emp = A[i]
+    A[i] = A[j]
+    A[j] = emp
+    return A
+
+
+mtx = [3,2,7,6,8,87,43,564,1,235,12,56] # a random vactor defined.
+print(mtx[0])
+print(bubbleSort(mtx))
