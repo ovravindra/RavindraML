6c20112aa84db9096a4d4fa942036f44f2ee37a9
ovravindra <60539897+ovravindra@users.noreply.github.com>
2020-02-09 22:10:56 +0530

    Add files via upload

--- cCipher.py
+++ cCipher.py
@@ -1,18 +1,17 @@
+# Caesar Cipher is crypting a message by shifting the letters
+
 def cCipher(str1, shift = 2):
     if (type(str1) == str):
-        # print('loc 1')
-        # str2 = []
-        str3 = []
+        
+        str3 = [] 
         for i in range(0,len(str1)):
-            # print('loc 2')
-            # str2[i] = ord('str1[i]') + shift
-            # x = str1[i]
-            # str2.append(ord(str(x)))
+
             str3.append(ord(str1[i]) + shift)
         
-        mystr = ''
+        mystr = '' # starting an empty string
+
         for i in str3:
-            mystr = mystr + chr(i)
+            mystr = mystr + chr(i) # converting the ascii numbers into characters and concatinating them
         return mystr
     else:
         print('the input is not a string')
